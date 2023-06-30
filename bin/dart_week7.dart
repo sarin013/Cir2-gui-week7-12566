2878ce33c6b504ffe98792ae5a89d89ea142e49b
sarin <sakrinthr240145@gmail.com>
2023-06-30 09:35:33 +0700

    first commit

---
+++ bin/dart_week7.dart
@@ -0,0 +1,11 @@
+void main(){
+  print("Logic Operator");
+  int x = 10;
+  int y = 9;
+  bool a = true;
+  bool b = true;
+
+  print("x==y = ${x==y}");//equal
+  print("a|b = ${a|b}");//OR gate
+  print("A&B = ${a&b}");// and gate
+}
